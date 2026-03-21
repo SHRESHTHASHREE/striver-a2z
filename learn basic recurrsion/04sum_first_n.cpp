a12bc3b8d6fff8817396b9a9608119de43187cc7
Shreshtha <1shreshthashree@gmail.com>
2026-03-21 17:47:19 +0530

    06

--- learn basic recurrsion/04sum_first_n.cpp
+++ learn basic recurrsion/04sum_first_n.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 using namespace std;
 
+
+// // s1:-
+// int main(){
+//     int n;
+//     cin>> n;
+//     int count =0;
+
+//     for (int i=1; i<=n ; i++){
+//         count+=i;
+      
+
+//     }
+//       cout << count ;
+
+// }
+//s2:-
+
+int  f( int n ){
+    if(n==0) return 0;
+
+    return n+ f(n-1);
+
+}
 int main(){
-    
+    int n;
+    cin>> n;
+
+    cout<< f(n);
+    return 0;
 }
