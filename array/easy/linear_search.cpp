5847105575271d63dc288012cfee5e4cbe816d74
Shreshtha <1shreshthashree@gmail.com>
2026-03-30 20:09:50 +0530

    array

---
+++ array/easy/linear_search.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int linear_search(vector<int>&arr, int n , int key){
+
+    for(int i=0; i<n ;i++){
+        if(arr[i]==key){
+            return i;
+        }
+    }
+    return -1;
+
+}
+
+int main(){
+    int n;
+    cin>>n;
+   
+
+    vector<int>arr(n);
+
+    for (int i=0; i<n; i++){
+        cin>> arr[i];
+
+    }
+    int key;
+    cin>>key;
+    
+    int ans =linear_search(arr,n,key);
+
+    cout<<ans;
+
+
+
+}
