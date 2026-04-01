0a012ea34248f54b19d38ca6ba7f8fc63bfb4dfd
Shreshtha <1shreshthashree@gmail.com>
2026-04-01 23:34:49 +0530

    easy

---
+++ array/medium/Kadane's Algorithm.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int maxsubarray(vector<int>&arr){
+    long long s=0;
+    for (int i=0; i<arr.size();i++){
+        s+=arr[i];
+
+    }
+}
+
+
+
+
+
+
+int main(){
+
+}
